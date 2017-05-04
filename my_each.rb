fda02befce556d0885a3dd9404c1b59d8759a284
Vera Zago <veranyc@gmail.com>
2017-05-04 12:41:18 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  x = 0
+  while x < collection.length
+      yield(collection[x])
+      x += 1
+  end
+  collection
 end
+
+my_each([1,2,3,4]){|x| print x}
