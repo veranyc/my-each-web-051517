eb3f592d24357d32464c53d4b23f7ddb449cf894
Vera Zago <veranyc@gmail.com>
2017-05-04 13:36:14 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,13 @@
 def my_each(collection)
-  x = 0
+  x = 0 #counter
   while x < collection.length
-      yield(collection[x])
-      x += 1
+    yield(collection[x])
+    #while counter is less than the number of elements in the array, run/yield
+    #to a block of code defined below in the parameters
+    x += 1 # add one to the counter
   end
-  collection
+  collection #return the array that was now changed
 end
 
 my_each([1,2,3,4]){|x| print x}
+#print each element of the array
